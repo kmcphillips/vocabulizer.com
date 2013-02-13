c983bb194f907dc7902ab1022f029e0ea1b54074
Kevin McPhillips <github@kevinmcphillips.ca>
2013-02-12 19:52:57 -0600

    Add tests for all the service stuff, wordnik specifically.

--- test/unit/term_test.rb
+++ test/unit/term_test.rb
@@ -38,23 +38,23 @@ class TermTest < ActiveSupport::TestCase
 
 
   test "class #base_value should not change anything if it's already a simple word" do
-    assert_equal Term.base_value("cake"), "cake"
+    assert_equal "cake", Term.base_value("cake")
   end
 
   test "class #base_value should scrub and set the base value on create" do
-    assert_equal Term.base_value("  TREES..."), "tree"
+    assert_equal "tree", Term.base_value("  TREES...")
   end
 
 
   test "#set_base_value should set the base value if it is not set" do
     Term.expects(:base_value).with("pie").returns("delicious")
     term = FactoryGirl.create(:term, value: "pie", base_value: nil)
-    assert_equal term.base_value, "delicious"
+    assert_equal "delicious", term.base_value
   end
 
   test "#set_base_value should not change anything if it is already set" do
     term = FactoryGirl.create(:term, value: "pie", base_value: "delicious")
-    assert_equal term.base_value, "delicious"
+    assert_equal "delicious", term.base_value
   end
 
   test "#set_base_value should return true since it is a callback, just to be safe" do
@@ -62,8 +62,55 @@ class TermTest < ActiveSupport::TestCase
   end
 
 
+  def mock_wordnik_result
+    [
+      {
+        "citations" => [
+            {"cite" => "cite 1a"},
+            {"cite" => "cite 1b"}
+          ],
+        "text" => "text 1",
+        "attributionText" => "text 1 attribution"
+      },
+      {
+        "citations" => [
+            {"cite" => "cite 2a"},
+            {"cite" => "cite 2b"}
+          ],
+        "text" => "text 2",
+        "attributionText" => "text 2 attribution"
+      },
+    ]
+  end
+
   test "#populate_from_wordnik should test the call to the service" do
-    pending "Test the service"
+    Wordnik.word.expects(:get_top_example).raises(ClientError, "No top example found")
+    Wordnik.word.expects(:get_definitions).with("pie", limit: 7, useCanonical: 'true').returns(mock_wordnik_result)
+    term = FactoryGirl.create(:term)
+    term.send(:populate_from_wordnik)
+    assert_equal 2, term.details.size
+  end
+
+  test "#populate_from_wordnik should set the expected data" do
+    Wordnik.word.expects(:get_top_example).raises(ClientError, "No top example found")
+    Wordnik.word.expects(:get_definitions).with("pie", limit: 7, useCanonical: 'true').returns(mock_wordnik_result)
+    term = FactoryGirl.create(:term)
+    term.send(:populate_from_wordnik)
+    detail = term.details.where(definition: "text 1").first
+    assert_not_nil detail
+    assert_equal "Wordnik / text 1 attribution", detail.source
+    assert_equal "cite 1a \ncite 1b", detail.example
+  end
+
+  test "#populate_from_wordnik should set the top example if it finds one" do
+    Wordnik.word.expects(:get_definitions).with("pie", limit: 7, useCanonical: 'true').returns(mock_wordnik_result)
+    Wordnik.word.expects(:get_top_example).returns("text" => "top example", "title" => "top attribution")
+    term = FactoryGirl.create(:term)
+    term.send(:populate_from_wordnik)
+    detail = term.details.where(top: true).first
+    assert_not_nil detail
+    assert_equal "Wordnik / top attribution", detail.source
+    assert_equal "top example", detail.example
   end
 
 
@@ -80,21 +127,21 @@ class TermTest < ActiveSupport::TestCase
     UrbanDictionary.expects(:define).with("pie").returns(mock_urban_result)
     term = FactoryGirl.create(:term)
     term.send(:populate_from_urban_dictionary)
-    assert_equal term.details.size, 3
+    assert_equal 3, term.details.size
   end
 
   test "#populate_from_urban_dictionary should limit to the number passed in" do
     UrbanDictionary.expects(:define).with("pie").returns(mock_urban_result)
     term = FactoryGirl.create(:term)
     term.send(:populate_from_urban_dictionary, 2)
-    assert_equal term.details.size, 2
+    assert_equal 2, term.details.size
   end
 
   test "#populate_from_urban_dictionary should pass if no entries are found" do
     UrbanDictionary.expects(:define).with("pie").returns(nil)
     term = FactoryGirl.create(:term)
     term.send(:populate_from_urban_dictionary)
-    assert_equal term.details.size, 0
+    assert_equal 0, term.details.size
   end
 
   test "#populate_from_urban_dictionary should pull in the expected fields" do
@@ -103,8 +150,8 @@ class TermTest < ActiveSupport::TestCase
     term.send(:populate_from_urban_dictionary)
     detail = term.details.where(definition: "a").first
     assert_not_nil detail
-    assert_equal detail.example, "example a"
-    assert_equal detail.source, "Urban Dictionary"
+    assert_equal "example a", detail.example
+    assert_equal "Urban Dictionary", detail.source
     assert detail.urban
   end
 
